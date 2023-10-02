2185322b5bdcc09987741aba04b3289c7ebecc9b
nahom1256 <theweeknd1256@gmail.com>
2023-10-02 11:28:49 -0700

    asdf

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,5 +3,11 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-void _puts(char *str){}
-
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
