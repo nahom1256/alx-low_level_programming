860aea0607d51bb26665cdccf0c29fa6071d18f4
nahom1256 <theweeknd1256@gmail.com>
2023-10-30 11:11:38 -0700

    1st

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * read_textfile - that reads
+ * @filename: pointer
+ * @letters: size letters
+ * Description: Write a function
+ * Return: the actual number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t f, l, u;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	l = read(f, text, letters);
+
+	u = write(STDOUT_FILENO, text, l);
+
+	close(f);
+
+	return (u);
+}
